9aeaee8e52f22ee74d161c329b2344c8e8c5d65d
Oli Morris <olimorris@users.noreply.github.com>
2022-07-05 22:52:23 +0100

    refactor: utils.lua

--- lua/neotest-phpunit/utils.lua
+++ lua/neotest-phpunit/utils.lua
@@ -50,30 +50,21 @@ end
 ---@param test_data table
 ---@return boolean
 local function is_method_test(test_data)
-  if test_data["_attr"].file and test_data.testcase["_attr"] then
-    return true
-  end
-  return false
+  return test_data["_attr"].file and test_data.testcase["_attr"]
 end
 
 ---Determine if the provided test is for a file
 ---@param test_data table
 ---@return boolean
 local function is_file_test(test_data)
-  if test_data["_attr"].file and not test_data.testcase["_attr"] then
-    return true
-  end
-  return false
+  return test_data["_attr"].file and not test_data.testcase["_attr"]
 end
 
 ---Determine if the provided test is for a directory
 ---@param test_data table
 ---@return boolean
 local function is_dir_test(test_data)
-  if not test_data["_attr"].file then
-    return true
-  end
-  return false
+  return not test_data["_attr"].file
 end
 
 --- Parse PHPUnits XML output and return a table of test results
@@ -87,7 +78,6 @@ M.parse_xml_output = function(parsed_xml_output, output_file)
     if is_method_test(testsuites) then
       local test_id, test_output = generate_test_output(testsuites.testcase, output_file)
       tests[test_id] = test_output
-      return tests
     end
 
     if is_file_test(testsuites) then
@@ -95,7 +85,6 @@ M.parse_xml_output = function(parsed_xml_output, output_file)
         local test_id, test_output = generate_test_output(testcase, output_file)
         tests[test_id] = test_output
       end
-      return tests
     end
 
     if is_dir_test(testsuites) then
@@ -103,12 +92,10 @@ M.parse_xml_output = function(parsed_xml_output, output_file)
         local test_id, test_output = generate_test_output(testcase, output_file)
         tests[test_id] = test_output
       end
-      return tests
     end
   end
 
-  logger.error("Could not get test results")
-  return {}
+  return tests
 end
 
 return M
