07f561e6883ef918feaf8d7a12b25e95c414b87c
Oli Morris <olimorris@users.noreply.github.com>
2022-07-06 23:37:56 +0100

    chore: add additional test examples

---
+++ tests/Examples/some/deep/nesting/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_something_that_is_true()
+    {
+       $this->assertTrue(true);
+    }
+}
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,11 +6,17 @@ class ExampleTest extends TestCase
 {
     public function test_that_true_is_true()
     {
-       $this->assertTrue(true);
+        $this->assertTrue(true);
     }
 
-    public function test_that_false_is_true()
+    /** @test */
+    public function this_should_fail()
     {
         $this->assertTrue(false);
+    }
+
+    public function this_should_not_run()
+    {
+        $this->assertTrue(true);
     }
 }
