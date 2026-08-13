600664bd4aac6cc80fb3802d8cec7dc53f8ea44f
nikola-maric-aula <nikola.maric@aula.de>
2026-08-13 09:37:39 +0000

    fix: update resolving of config options

--- lua/neotest-phpunit/config.lua
+++ lua/neotest-phpunit/config.lua
@@ -20,7 +20,7 @@ M.get_filter_dirs = function()
   return { ".git", "node_modules" }
 end
 
-M.is_test_file = function(file_path)
+M.get_is_test_file = function(file_path)
   return vim.endswith(file_path, "Test.php")
 end
 
